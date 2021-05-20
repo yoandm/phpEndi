b3af938f180cc77c8df8b5005bcce3c15acb74cb
yoandm <mail@yoandm.com>
2021-05-20 16:39:09 +0200

    Documentation démo

--- examples/demo.php
+++ examples/demo.php
@@ -6,8 +6,12 @@
 	use yoandm\phpEndi\Customer;
 	use yoandm\phpEndi\Invoice;
 
+	// Remplacer 1000 par l'identiant de votre structure dans enDI
+	// Si vous allez dans "Mon enseigne" et que vous jetez un oeil à l'URL de la page
+	// vous verrez companies/xxxx. C'est ce xxxx qui nous intéresse
 	phpEndi::connect('email@domain.com', 'password', 'mydomain.fr', 1000); 
 
+	// Pour ajouter un client
 	$client = Customer::add([
 				'code' => '',
 				'company_name' => 'test',
@@ -15,9 +19,9 @@
 				'lastname' => '',
 				'firstname' => '',
 				'function' => '',
-				'address' => 'rue test',
-				'zip_code' => '63000',
-				'city' => 'Clermont-Ferrand',
+				'address' => '17 rue de la tour',
+				'zip_code' => '75002',
+				'city' => 'Paris',
 				'country' => '',
 				'tva_intracomm' => '',
 				'registration' => '',
@@ -28,15 +32,24 @@
 				'comments' => ''
 	]);
 	
-	Customer::addToProject($client, 142874);
+	// Pour ajouter le client à un projet
+	// remplacez 10 par l'identifiant de votre projet (dossier)
+	// Vente / Dossier, vous sélectionnez votre projet
+	// Vous verrez une URL du type projects/xxxx/invoices
+	// C'est ce xxxx qui nous intéresse ici
+	Customer::addToProject($client, 10);
 
-	$facture = Invoice::add($client, 142874, 'Ma super facture');
+	// Création d'une facture
+	$facture = Invoice::add($client, 10, 'Ma super facture'); // remplacez 10 par l'identifiant de votre projet
 
+	// Mise à jour de l'objet de la facture
 	Invoice::setObject($facture, 'Ma super facture');
+
+	// Pour que la facture affiche le détail
 	Invoice::setDisplayUnit($facture, 1);
 
+	// Ajout d'une ligne dans la facture
 	$lgroups = Invoice::getTaskLineGroups($facture);
-
 	Invoice::addLine($facture, [
 
 								'order' => 1,
@@ -52,12 +65,15 @@
 
 	);
 
+	// Conditions de paiement spécifique
 	Invoice::setPaymentConditions($facture, 'Paiement par CB');
 
+	// Enregistrement de la facture modifiée
 	Invoice::save($facture, [
 								'name' => 'Ma super facture',
 								'financial_year' => date('Y')	
 						]
 	);
 
+	// Fermeture de la session
 	phpEndi::destroySession();
